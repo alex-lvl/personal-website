a4cc3f3c49646ad69581cd3205f24a17fb0959b5
Alex Marmolejo <alex97marmolejo@gmail.com>
2023-03-02 03:01:26 -0500

    edited about me paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,16 @@ function About() {
       <h1>about</h1>
       <hr></hr>
       <p>
-        Hello! My name is Alex and I am a Full-Stack Javascript Developer based
-        in New York. I am particularly focused on the MERN stack (MongoDB,
-        Express.js, React, and Node.js) and am skilled in creating visually
-        appealing web pages with attention to detail. My background in
-        accounting, specifically my knowledge of finance and financial markets
-        as well as my experience with taxes, allows me to bring a unique
-        perspective to web development. I believe programming is a powerful tool
-        that can be used to create technology that simplifies people's lives and
-        I am excited to pursue a career in web development to create technology
-        for the world!
+        Hello! My name is Alex and I am a self-taught Full-Stack Javascript
+        Developer based in New York. I am particularly focused on the MERN stack
+        (MongoDB, Express.js, React, and Node.js) and am skilled in creating
+        visually appealing web pages with attention to detail. My educational
+        background in accounting, specifically my experience with taxes, as well
+        as my knowledge of finance and financial markets, allows me to bring a
+        unique perspective to web development. I believe programming is a
+        powerful tool that can be used to create technology that simplifies
+        people's lives and I am excited to pursue a career in web development to
+        create technology for the world!
       </p>
     </div>
   );
